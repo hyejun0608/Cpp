ab8bed4c5772290767200ae638fb0e8e9be70e27
hyejun <hyejun0608@naver.com>
2020-10-08 08:46:53 +0900

    Deep Copy
    깊은 복사

---
+++ Copy Constructor/Deep Copy.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class CMyData {
+public:
+	CMyData() {
+		m_pnData = new int;
+		*m_pnData = 0;
+	}
+	CMyData(int nParam) {
+		cout << "CMyData(int) " << endl;
+		m_pnData = new int;
+		*m_pnData = nParam;
+	}
+	CMyData(const CMyData& rhs) {
+		cout << "CMyData(const CMyData& rhs)" << endl;
+		// 메모리를 할당한다
+		m_pnData = new int;
+
+		// 포인터가 가르키는 위치에 값을 복사한다.
+		*m_pnData = *rhs.m_pnData;
+	}
+	// 객체가 소멸하면 동적 할당한 메모리를 해제한다
+	~CMyData() {
+		delete m_pnData;
+	}
+	int GetData() const {
+		if (m_pnData != NULL) return *m_pnData;
+		return 0;
+	}
+
+private:
+	// 포인터 멤버 데이터
+	int* m_pnData = nullptr;
+};
+
+int main() {
+	CMyData a(10);
+	CMyData b(a);
+	cout << a.GetData() << endl;
+	cout << b.GetData() << endl;
+
+	return 0;
+}
