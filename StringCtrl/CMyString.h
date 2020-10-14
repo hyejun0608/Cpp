06ede45c6bfc4ec9c948ea7174e6855895221fba
hyejun <hyejun0608@naver.com>
2020-10-14 12:08:37 +0900

    CMyString.h

--- StringCtrl/CMyString.h
+++ StringCtrl/CMyString.h
@@ -1,11 +1,13 @@
 #pragma once
-class CMyString
-{
+class CMyString {
 public:
 	CMyString();
+	// 복사 생성자
+	CMyString(const CMyString& rhs);
+	CMyString& operator = (const CMyString& rhs);
 	~CMyString();
 private:
-	// 문자열을 저장하기 위해 동적 할당한 메모리를 가르키는 포인터
+	// 문자열을 저장하기 위해 동적 할당한 메모리를 가리키는 포인터
 	char* m_pszData;
 	// 저장된 문자열의 길이
 	int m_nLength;
@@ -13,5 +15,12 @@ public:
 	int SetString(const char* pszParam);
 	const char* GetString() const;
 	void Release();
-};
+	// 변환 생성자
+	explicit CMyString(const char* pszParam);
+	operator char* () const {
+		return	m_pszData;
+	}
+	// 이동생성자
+	CMyString(CMyString&& rhs);
 
+};
