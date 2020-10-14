7a031d540a90eb352eb82f61440a96aada4ccd49
hyejun <hyejun0608@naver.com>
2020-10-14 12:09:53 +0900

    StringCtrlSample1107.cpp

--- StringCtrl/StringCtrlSample1107.cpp
+++ StringCtrl/StringCtrlSample1107.cpp
@@ -2,26 +2,16 @@
 #include "CMyString.h"
 using namespace std;
 
-void TestFunc(const CMyString& param) {
-	cout << param.GetString() << endl;
+CMyString TestFunc() {
+	CMyString strTest("TestFunc() return");
+	cout << strTest << endl;
+
+	return strTest;
 }
 
 int main() {
-	CMyString strData;
-	cout << strData.GetString() << endl;
-	strData.Release();
-	cout << strData.GetString() << endl;
-
-	CMyString strData2;
-	strData2.SetString("");
-	cout << strData2.GetString() << endl;
-	strData2.Release();
-
-	CMyString strData3;
-	strData3.SetString("Hello");
-	TestFunc(strData3);
-	cout << strData3.GetString() << endl;
-	strData3.Release();
+	// 이름없는 임시 객체가 만들어진다.
+	TestFunc();
 
 	return 0;
 }
